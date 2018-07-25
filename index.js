ec1fcead6dd6e14ec62227648806a3853a0a0659
Fernanda Gonzalez <fer.ro.mayela@gmail.com>
2018-07-25 18:32:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 
 function getUKTime(yourTimeZone) {
-  new getHours(yourTimeZone) + 5
+  new Date(yourTimeZone).getHours(yourTimeZone) + 5
 }
  
 getUKTime(yourTimeZone)
